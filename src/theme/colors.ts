23bff85f542f260249d1bfa23d212beb23fe20bf
cronofi <cronoking@gmail.com>
2022-01-06 16:16:32 +0800

    .

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -37,7 +37,7 @@ export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
   secondary: "#9A6AFF",
-  background: "#1e293b",
+  background: "#000b18",
   backgroundDisabled: "#27262c",
   contrast: "#1e293b",
   invertedContrast: "#191326",
@@ -48,7 +48,7 @@ export const darkColors: Colors = {
   textDisabled: "#666171",
   textSubtle: "#c9c4d4",
   borderColor: "#000000",
-  card: "#1e293b",
+  card: "#000b18",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },
